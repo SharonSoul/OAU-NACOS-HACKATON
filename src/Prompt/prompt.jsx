02bf5e3b6fedca9ca7cd0d6c321ac92d4bfb1255
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-05-01 01:53:56 +0100

    prompt media responsiveness

--- src/Prompt/prompt.jsx
+++ src/Prompt/prompt.jsx
@@ -5,9 +5,9 @@ Link
 const Research = () => {
 	return (
 		<div className='h-[100vh] bg-slate-700 pt-4'>
-			<div className='bg-slate-900 w-[70%] m-auto rounded-md p-3'>
-				<div className=' text-white    '>
-					<ul className='flex justify-between p-4'>
+			<div className='bg-slate-900 w-[90%] md:w-[70%] m-auto rounded-md md:p-3'>
+				<div className=' text-white   text-sm md:text-base '>
+					<ul className='flex justify-between p-2 md:p-4'>
 						<li className=''><Link to="">Overview</Link></li>
 						<li><Link to="">Citations</Link></li>
 						<li><Link to="">Images</Link></li>
@@ -16,10 +16,10 @@ const Research = () => {
 					</ul>
 				</div>
 				<div className=''>
-					<div className='flex gap-1 mt-2 ml-2 justify-center'>
-						<input type="number" name="" id="" className='rounded-l-md w-[110px] pl-3 text-base' placeholder='Word Count' />
-						<input type="text" name="UserInput" id="" className='w-[300px] text-base pl-3' placeholder='Enter Prompt' />
-						<button className='bg-yellow-100 text-slate-900 font-bold p-2  w-[100px]'>Send</button>
+					<div className=' m-5 pb-3 flex flex-col align-middle md:flex-row md:gap-1 md:mt-2 md:ml-2 justify-center'>
+						<input type="number" name="" id="" className='rounded-l-md  md:w-[110px] pl-3 text-sm md:text-base mb-3 md:mb-0 p-3 ' placeholder='Word Count' />
+						<input type="text" name="UserInput" id="" className='md:w-[300px] text-base pl-3 mb-3 md:mb-0 p-3' placeholder='Enter Prompt' />
+						<button className='bg-yellow-100 text-slate-900 font-bold p-2  w-[100px] block m-auto'>Send</button>
 					</div>
 				</div>
 			</div>
